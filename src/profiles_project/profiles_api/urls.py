2f8b13975737a704a3e20f442a285f2660427154
mohamed naski <mohamed.naski@supcom.tn>
2023-04-07 23:36:08 +0100

    Added the first viewset and router

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -2,6 +2,17 @@ from django.conf.urls import url
 
 from . import views
 
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+
+#this for the ViewSet
+router = DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet, base_name='hello-viewset')
+
+#this for APIView
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
+    url(r'',include(router.urls))
 ]
+
+
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -55,3 +56,18 @@ class HelloApiView(APIView):
         """delete request, only delete fields provided in the request."""
 
         return Response({'method': 'delete'})
+    
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet."""
+
+    def list(self,request):
+        """Return a hello message."""
+
+        a_viewset = [
+            'Uses actions (list,create,retrieve,update,partial_update)',
+            'Automatically maps to URLs using Rootes',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message':'Hello!','a_viewset':a_viewset})
